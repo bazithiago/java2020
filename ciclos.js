74f2589b7a4e7cd3389dc3819288b97a1e77bcca
Thiago <thiagobazinga@gmail.com>
2020-05-27 19:48:32 -0300

    Passo a passos

--- ciclos.js
+++ ciclos.js
@@ -1,17 +1,26 @@
-console.log("2) O computador repete para nós")
+// console.log("2) O computador repete para nós")
 
-function imprimirAzul4() {
-    for (var i = 0; i < 4; i++) {
-        console.log("Azul")
-    }
+// function imprimirAzul4() {
+//     for (var i = 0; i < 4; i++) {
+//         console.log("Azul")
+//     }
+// }
+// imprimirAzul4();
+
+// console.log("3) Reforçamos o que vimos")
+
+// function imprimir5vezes5() {
+//     for (var i = 0; i < 5; i++) {
+//         console.log("5")
+//     }
+// }
+// imprimir5vezes5();
+
+console.log("4) Passo a passo:")
+
+function passoAPasso() {
+    for(var i = 0; i < 5; i++) {
+        console.log(i)
+     }
 }
-imprimirAzul4();
-
-console.log("3) Reforçamos o que vimos")
-
-function imprimir5vezes5() {
-    for (var i = 0; i < 5; i++) {
-        console.log("5")
-    }
-}
-imprimir5vezes5();
+passoAPasso()
