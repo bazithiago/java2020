56759c8f7666a757293bbf214632441eea08dc9e
Thiago <thiagobazinga@gmail.com>
2020-05-27 19:44:17 -0300

    Primeiros exercicios

---
+++ ciclos.js
@@ -0,0 +1,17 @@
+console.log("2) O computador repete para nós")
+
+function imprimirAzul4() {
+    for (var i = 0; i < 4; i++) {
+        console.log("Azul")
+    }
+}
+imprimirAzul4();
+
+console.log("3) Reforçamos o que vimos")
+
+function imprimir5vezes5() {
+    for (var i = 0; i < 5; i++) {
+        console.log("5")
+    }
+}
+imprimir5vezes5();
