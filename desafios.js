6db7a9c325ef7cd2db4c6b29d09e5c46aa531e8a
Thiago <thiagobazinga@gmail.com>
2020-05-29 16:45:02 -0300

    Produto

--- desafios.js
+++ desafios.js
@@ -1,65 +1,11 @@
-console.log("Desafio Professora Furiosa:")
+console.log("Desafio Produto:")
 
-let alunosDeSegunda = [10, -5, -3, -1]
-let alunosDeTerca = [7, -5, -3, ,3]
-let alunosDeQuarta = [10, -5, -3, 1,2]
-
-function acontece(diaDaSemana, quantidadeMinima) {
-
-    let quantidadeAlunos = 0;
-
-    for (let i = 0; i < diaDaSemana.length; i++) {
-
-        if (diaDaSemana[i]>=0) quantidadeAlunos++;
-        if (quantidadeAlunos >= quantidadeMinima) return true;
-    }
-    return false
+function produto(array) {
+    var resultado = 1;
+    for (var i = 0; i < array.length; i++) {
+        resultado *= array[i];
+    } 
+    return resultado;
 }
 
-function aberturas(diaDaSemana,quantidadeMinima) {
-    let listaCompleta = [];
-
-    for (let i = 0; i < diaDaSemana.length; i++) {
-        
-        listaCompleta.push(acontece(diaDaSemana[i], quantidadeMinima));
-    }
-    return listaCompleta;
-}
-
-console.log(aberturas([alunosDeSegunda,alunosDeTerca,alunosDeQuarta], 2))
-
-
-
-// A MINHA DEU CERTO AQUI MAS ERRADO LÁ, COLEI DO BRAINLY, MAS SEM ENTENDER O QUE TA DIFERENTE
-
-function acontece (alunosDia, quorum){
-
-       let presentes = 0;
-    
-       for (let i = 0; i < alunosDia.length; i++){
-    
-         if (alunosDia[i] <= 0) presentes++;  
-    
-         if (presentes >= quorum) return true;
-    
-       }
-    
-       return false;  
-    
-    }
-    
-    function aberturas (alunosDia, quorum){
-    
-       let resultadoArray = [];
-    
-       for (let i = 0; i < alunosDia.length; i++){
-    
-        resultadoArray.push(acontece(alunosDia[i], quorum));
-    
-       }
-    
-       return resultadoArray;  
-    
-    }
-    
-    Leia mais em Brainly.com.br - https://brainly.com.br/tarefa/24484444#readmore
+console.log(produto([1, 4, 7]));
