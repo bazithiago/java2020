791f8ca2b067871985f4ca36ab0c03860184c730
Thiago <thiagobazinga@gmail.com>
2020-05-29 16:51:15 -0300

    Mais ou menosl

--- desafios.js
+++ desafios.js
@@ -1,12 +1,24 @@
-console.log("Desafio Fatorial:")
+console.log("maisMenos")
 
-function fatorial(numero) {
-    var resultado = 1;
+function maisMenos(array) {
+    let positivos = [];
+    let zeros = [];
+    let negativos = [];
 
-    for (var i = 1; i < numero; i++) {
-        resultado *= i+1;
-    } 
-    return resultado;
+
+    for (let i = 0; i < array.length; i++) {
+        if(array[i] > 0) {
+            positivos.push(array[i]);
+        } else if (array[i] == 0) {
+            zeros.push(array[i]);
+        } else {
+            negativos.push(array[i]);
+        }
+    }
+    
+    let compilados = [(positivos.length/array.length) , (zeros.length/array.length) , (negativos.length/array.length)];
+
+    return compilados;
 }
 
-console.log(fatorial(4));
+console.log(maisMenos([1, 2, 0, -1]))
